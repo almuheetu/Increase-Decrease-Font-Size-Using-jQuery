59caeb55b4e48ccc07b5c2b691260b020573aae9
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-01 23:54:17 +0600

    cdn fixed

--- mian.js
+++ mian.js
@@ -1,7 +1,8 @@
-$(document).redy(function() {
-    $("#range-slider").on("input change", function() {
-        $(".text").css("font-size", $(this).val() + "px");
-
-        //change span value
-    })
-})
+$(document).ready(function () {
+    $("#range-slider").on("input change", function () {
+      $(".text").css("font-size", $(this).val() + "px");
+      //change span value
+      $(".value").text($(this).val() + "px");
+    });
+  });
+  
